c09b41d27744a6701618de7ffd4263ca9c32c0dd
Hamza <hamzayousfi2@gmail.com>
2020-03-25 18:30:08 +0200

    final styling (majorly css)

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ io.on("connection", socket => {
 
     socket.emit("message", {
       user: "admin",
-      text: `${user.name}, welcome to the room ${user.room}`
+      text: `${user.name}, welcome to our room ${user.room}`
     });
 
     socket.broadcast
