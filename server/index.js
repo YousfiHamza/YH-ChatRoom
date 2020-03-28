7e97f75dc43dca8785e2dd42cf3342de72801b93
Hamza <hamzayousfi2@gmail.com>
2020-03-28 13:45:31 +0200

    setting up Cross-origin resource sharing

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
 
 const PORT = process.env.PORT || 5000;
 
 const router = require("./router");
+app.use(cors());
 
 const app = express();
 const server = http.createServer(app);
